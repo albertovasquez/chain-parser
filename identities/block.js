b0d1494fff783b022a2f2d131b962f2fffd3d0cc
Alberto Vasquez <albertovasquez@aveix-mbp.lan>
2020-06-23 11:33:46 -0400

    Working on additional block information

--- identities/block.js
+++ identities/block.js
@@ -1,26 +1,52 @@
 const client = require('../services/bitcoin-core');
 const Transaction = require('./transaction');
 const db = require('../services/db');
+const converter = require('hex2dec');
 
 const createBlock = ({
-    height = null,
-    blockHash = null,
-    rawBlock=null,
-    blockTime = null,
+    height,
+    blockHash,
+    rawBlock,
+    blockTime,
+    version,
+    nonce,
+    difficulty,
+    merkleroot,
+    bits,
+    size,
+    weight
 } = {}) => {
-    if (height === null || height === undefined) {
+    if (weight === undefined) {
+        throw new Error('weight is required');
+    }
+    if (size === undefined) {
+        throw new Error('size is required');
+    }
+    if (merkleroot === undefined) {
+        throw new Error('merkleroot is required');
+    }
+    if (bits === undefined) {
+        throw new Error('bits is required');
+    }
+    if (height === undefined) {
         throw new Error('height is required');
     }
-
-    if (!blockTime) {
+    if (difficulty === undefined) {
+        throw new Error('difficulty is required');
+    }
+    if (nonce === undefined) {
+        throw new Error('nonce is required');
+    }
+    if (version === undefined) {
+        throw new Error('version is required');
+    }
+    if (blockTime === undefined) {
         throw new Error('blockTime is required');
     }
-
-    if (!blockHash) {
+    if (blockHash === undefined) {
         throw new Error('hash is required');
     }
-
-    if (!rawBlock) {
+    if (rawBlock === undefined) {
         throw new Error('block is required');
     }
 
@@ -28,7 +54,7 @@ const createBlock = ({
         getHeight: () => height,
         removeFromDb: async () => {
             // If we don't have the block return
-            const block = await db('block').where({hash: blockHash})
+            const block = await db('block').where({ hash: blockHash })
             if (!block.length) return;
 
             // remove all transactions in block
@@ -43,10 +69,21 @@ const createBlock = ({
             }
 
             // remove block
-            await db('block').where({hash: blockHash}).del();
+            await db('block').where({ hash: blockHash }).del();
         },
         parseToDb: async () => {
-            const [blockid] = await db('block').insert({hash: blockHash, blocktime: blockTime});
+            const [blockid] = await db('block').insert({
+                height,
+                hash: blockHash,
+                blocktime: blockTime,
+                nonce,
+                bits,
+                difficulty,
+                version,
+                merkleroot,
+                weight,
+                size,
+            });
 
             for (const hash of rawBlock.tx) {
                 try {
@@ -68,10 +105,24 @@ module.exports = {
         try {
             hash = await client.getBlockHash(height);
             rawBlock = await client.getBlock(hash);
+            // console.log(rawBlock);
+            // converter.hexToDec(rawBlock.bits)
         } catch (ex) {
             throw ex;
         }
 
-        return createBlock({height, blockHash: hash, rawBlock, blockTime: rawBlock.time});
+        return createBlock({
+            height,
+            bits: rawBlock.bits,
+            blockHash: hash,
+            rawBlock,
+            weight: rawBlock.weight,
+            size: rawBlock.size,
+            merkleroot: rawBlock.merkleroot,
+            blockTime: rawBlock.time,
+            nonce: rawBlock.nonce,
+            difficulty: rawBlock.difficulty,
+            version: rawBlock.difficulty,
+        });
     },
 }
